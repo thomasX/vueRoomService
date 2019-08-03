375f3e47fe9e4337db796f5fbdb15e577f5a29f7
Thomas Margreiter <tm@felder.at>
2019-08-03 19:42:29 +0200

    server added

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,16 @@
-console.log('hello i am the server 6')
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyparser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world'
+  })
+})
+app.listen(process.env.PORT || 8086)
