bb90479e7bf071949c1082c71eb6574db91d4605
f3thomas <tm@felder-group.com>
2019-08-29 18:49:48 +0200

    update

--- server/src/ScrollBO/AbstractScrollBO.js
+++ server/src/ScrollBO/AbstractScrollBO.js
@@ -1,6 +1,22 @@
 module.exports = class AbstractScrollBO {
   getScrollModel (scrollBO, scrollrequest, user) {
-    lll
-    return 'zuzu' + user
+    const lines = collectLines(scrollBO,scrollrequest,user)
+    return lines
+  }
+
+  collectLines (scrollBO, scrollrequest, user) {
+    let firstDataReached = false
+    let lastDataReached = false
+    let startingLine = scrollrequest.start
+    if (scrollrequest.direction === true) {
+        if ((startingLine === undefined) || (startingLine === null ) {
+          startingLine = findFirst(scrollBO,scrollrequest,user)
+          firstDataReached = true
+        }
+        ...
+    } else {
+
+    }
+    return lines
   }
 }
