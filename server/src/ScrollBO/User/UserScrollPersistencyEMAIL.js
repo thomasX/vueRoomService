29a63c2b77c82552b906face8af48e22e803ec06
f3thomas <tm@felder-group.com>
2019-09-25 19:31:40 +0200

    update

--- server/src/ScrollBO/User/UserScrollPersistencyEMAIL.js
+++ server/src/ScrollBO/User/UserScrollPersistencyEMAIL.js
@@ -1,31 +1,31 @@
 module.exports = class UserScrollPersistencyEMAIL {
   createFirstStatement (filter) {
-    const statement = 'select * from Users where 1 = 1 #filter# order by email asc Limit #rows#'
+    let statement = 'select * from Users where 1 = 1 #filter# order by email asc Limit #rows#'
     const filterstatement = this.createFilterstatement(filter)
-    statement.replace(/#filter#/, filterstatement)
+    statement = statement.replace(/#filter#/, filterstatement)
     return statement
   }
 
   createLastStatement (filter) {
-    const statement = 'select * from Users where 1 = 1 #filter# order by email desc Limit #rows#'
+    let statement = 'select * from Users where 1 = 1 #filter# order by email desc Limit #rows#'
     const filterstatement = this.createFilterstatement(filter)
-    statement.replace(/#filter#/, filterstatement)
+    statement = statement.replace(/#filter#/, filterstatement)
     return statement
   }
 
   createForwardStatement (start, filter, rows) {
-    const statement = 'select * from Users where 1 = 1 #filter# order by email asc Limit #rows#'
+    let statement = 'select * from Users where 1 = 1 #filter# order by email asc Limit #rows#'
     const filterstatement = this.createFilterstatement(filter)
-    statement.replace(/#filter#/, filterstatement)
-    statement.replace(/#rows#/, rows)
+    statement = statement.replace(/#filter#/, filterstatement)
+    statement = statement.replace(/#rows#/, rows)
     return statement
   }
 
   createBackwardStatement (start, filter, rows) {
-    const statement = 'select * from Users where 1 = 1 #filter# order by email desc Limit #rows#'
+    let statement = 'select * from Users where 1 = 1 #filter# order by email desc Limit #rows#'
     const filterstatement = this.createFilterstatement(filter)
-    statement.replace(/#filter#/, filterstatement)
-    statement.replace(/#rows#/, rows)
+    statement = statement.replace(/#filter#/, filterstatement)
+    statement = statement.replace(/#rows#/, rows)
     return statement
   }
 
